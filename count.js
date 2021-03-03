79aefe7a384f09554b6a6e4bdd21deba52a3377b
LeBarney <barneywarframe@gmail.com>
2021-03-03 17:13:03 +0100

    modif

--- count.js
+++ count.js
@@ -1,7 +1,9 @@
-var compteur = function(min, max, step){
-for (let i = min; i <= max; i += step){
+var compteur = function(min, max, step)
+{
+for (let i = min; i <= max; i += step)
+  {
   console.log(i) 
-}
+  }
 }
 
 compteur(1,1000,10)
