93350d1d7dea95ab8a03a0ce559d6c0e5b730fd5
LeBarney <barneywarframe@gmail.com>
2021-03-04 17:57:25 +0100

    calculatrice finie

--- calc.js
+++ calc.js
@@ -3,4 +3,35 @@ const add = (nb1,nb2) => {
   return somme;
 }
 
-console.log(add(2,4));
+const sub = (nb1,nb2) => {
+  let reste = nb1 - nb2;
+  return reste;
+}
+
+const mul = (nb1,nb2) => {
+  let resultat = nb1 * nb2;
+  return resultat;
+}
+
+const div = (nb1,nb2) => {
+  let quotient = nb1 / nb2;
+  return quotient;
+}
+
+const calc = (symbol,nb1,nb2) => {
+  if (symbol === '+'){
+    return add(nb1,nb2);
+  }
+  if (symbol === '-'){
+    return sub(nb1,nb2);
+  }
+  if (symbol === '*'){
+    return mul(nb1,nb2);
+  }
+  if (symbol === '/'){
+    return div(nb1,nb2);
+  }
+}
+
+
+console.log(calc('*',2,4));
